0f959aa054e86e8c4845f5269d8f1101aafd79ee
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-03-23 18:21:15 +0100

    footer continuous commit

--- FrontEnd/src/Components/Footer/Footer.jsx
+++ FrontEnd/src/Components/Footer/Footer.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
-import './Footer.css'
-
-
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
-
-
   return (
-    <div className='footer'>
-      
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+            praesentium cumque minima, soluta voluptate quos. dolor sit amet
+            consectetur, adipisicing elit. Quisquam praesentium cumque minima,
+            soluta voluptate quos.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-243-433-4422</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 &copy; Tomato.com - All Right Reserved
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
