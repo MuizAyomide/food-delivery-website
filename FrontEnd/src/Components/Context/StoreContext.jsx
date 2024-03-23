d837b2697db0b4fdfee87e1f63d64563bf5214ab
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-03-23 05:20:30 +0100

    commit

--- FrontEnd/src/Components/Context/StoreContext.jsx
+++ FrontEnd/src/Components/Context/StoreContext.jsx
@@ -1,11 +1,31 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreCongtextProvider = (props) => {
+
+const [cartItems,setCartItems] = useState({});
+
+const addToCart = (itemId) => {
+  if (!cartItems[itemId]){
+    setCartItems((prev)=>({...prev,[itemId]:1}))
+  }
+  else {
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+  }
+}
+
+const removeFormCart = (itemId) => {
+  setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+}
+
   const contextValue = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFormCart
   };
   return (
     <StoreContext.Provider value={contextValue}>
--- FrontEnd/src/Components/FoodItems/FoodItems.jsx
+++ FrontEnd/src/Components/FoodItems/FoodItems.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
-import './FoodItems.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import "./FoodItems.css";
+import { assets } from "../../assets/assets";
 
-const FoodItems = ({id,name,price,description,image}) => {
+const FoodItems = ({ id, name, price, description, image }) => {
+    
+  const [itemCount, setItemCount] = useState(0);
 
   return (
-    <div className='food-item'>
-        <div className="food-item-img-container">
-            <img className='food-item-image' src={image} alt="" />
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-image" src={image} alt="" />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
         </div>
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
-            </div>
-            <p className='food-item-desc'>{description}</p>
-            <p className='food-item-price'>${price}</p>
-        </div>
-      
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItems
+export default FoodItems;
