dda4483fb31e3da62e607cfe7ed98df58b4f8024
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-27 11:32:52 +0100

    commit

--- FrontEnd/src/Components/Context/StoreContext.jsx
+++ FrontEnd/src/Components/Context/StoreContext.jsx
@@ -20,16 +20,24 @@ const StoreCongtextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
   }
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+const getTotalCartAmount = (item) => {
+  let totalAmount = 0;
+  for(const item in cartItems){
+    if (cartItems[item]>0){
+    let itemInfo = food_list.find((product)=>product._id === item);
+    totalAmount += itemInfo.price* cartItems[item];
+    }
+  }
+  return totalAmount;
+}
 
   const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
-    removeFormCart
+    removeFormCart,
+    getTotalCartAmount
   }
   return (
     <StoreContext.Provider value={contextValue}>
