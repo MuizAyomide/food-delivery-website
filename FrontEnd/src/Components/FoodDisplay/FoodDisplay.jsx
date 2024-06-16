0b239198313900ba8d194341464d90c4447a5e32
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-16 20:41:49 +0100

    commit

--- FrontEnd/src/Components/FoodDisplay/FoodDisplay.jsx
+++ FrontEnd/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -4,7 +4,6 @@ import { StoreContext } from "../Context/StoreContext";
 import FoodItems from "../FoodItems/FoodItems";
 
 const FoodDisplay = ({ category }) => {
-    
   const { food_list } = useContext(StoreContext);
 
   return (
@@ -12,8 +11,7 @@ const FoodDisplay = ({ category }) => {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-
-          if (category==='All' || category===item.category){
+          if (category === "All" || category === item.category) {
             return (
               <FoodItems
                 key={index}
@@ -24,9 +22,7 @@ const FoodDisplay = ({ category }) => {
                 image={item.image}
               />
             );
-
           }
-
         })}
       </div>
     </div>
