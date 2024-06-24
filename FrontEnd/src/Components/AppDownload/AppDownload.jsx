c7af89ccaacb328234a8e03203eb4576ae34773e
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-24 22:19:55 +0100

    commit

--- FrontEnd/src/Components/AppDownload/AppDownload.jsx
+++ FrontEnd/src/Components/AppDownload/AppDownload.jsx
@@ -9,6 +9,7 @@ const AppDownload = () => {
         For Better Experience Download <br />
         Tomato App
       </p>
+      
       <div className="app-download-platforms">
         <img src={assets.play_store} alt="" />
         <img src={assets.app_store} alt="" />
